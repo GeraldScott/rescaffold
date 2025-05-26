f8b15d6b433394570e1c159cdd1454349512d191
Gerald Scott <gerald@archton.io>
2025-05-26 12:26:34 +0200

    Restructure layout

--- src/main/java/io/archton/scaffold/web/GenderTemplate.java
+++ src/main/java/io/archton/scaffold/web/GenderTemplate.java
@@ -2,6 +2,7 @@ package io.archton.scaffold.web;
 
 import io.archton.scaffold.domain.Gender;
 import io.archton.scaffold.repository.GenderRepository;
+import io.archton.scaffold.resource.GenderResource;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
@@ -10,6 +11,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +19,8 @@ import java.util.Map;
 
 @Path("/genders-ui")
 public class GenderTemplate {
+
+    private static final Logger log = Logger.getLogger(GenderResource.class);
 
     @Inject
     GenderRepository genderRepository;
@@ -29,6 +33,7 @@ public class GenderTemplate {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String get() {
+        log.debug("GET /api/genders");
         List<Gender> genderList = genderRepository.listSorted();
         return Templates.genders(genderList).render();
     }
