6b53980d589e0c8a41e3f09fcb12e7526ca0eaf6
Gerald Scott <gerald@archton.io>
2025-05-31 19:13:45 +0200

    Add landing page

---
+++ src/main/java/io/archton/scaffold/web/HomeTemplate.java
@@ -0,0 +1,27 @@
+package io.archton.scaffold.web;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+
+@Path("/")
+public class HomeTemplate {
+
+    private static final Logger log = Logger.getLogger(HomeTemplate.class);
+
+    @CheckedTemplate(basePath = "")
+    public static class Templates {
+        public static native TemplateInstance home();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String get() {
+        log.debug("GET / - Home page");
+        return Templates.home().render();
+    }
+}
